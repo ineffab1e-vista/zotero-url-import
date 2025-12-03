0b69901aa1833ee52391d57eb3d3924ee0ff7ca1
Hanzhi Yin <yinhanzhi@gmail.com>
2025-12-03 10:55:45 +0800

    clean up hooks and index

--- src/hooks.ts
+++ src/hooks.ts
@@ -1,3 +1,6 @@
+import { registerURLImportHandler } from './modules/url-import';
+import { getString, initLocale } from './utils/locale';
+import { createZToolkit } from './utils/ztoolkit';
 // import {
 //   BasicExampleFactory,
 //   HelperExampleFactory,
@@ -5,10 +8,7 @@
 //   PromptExampleFactory,
 //   UIExampleFactory,
 // } from "./modules/examples";
-import { registerURLImportHandler } from './modules/url-import';
-import { getString, initLocale } from './utils/locale';
 // import { registerPrefsScripts } from "./modules/preferenceScript";
-import { createZToolkit } from './utils/ztoolkit';
 
 async function onStartup() {
   await Promise.all([
@@ -18,23 +18,6 @@ async function onStartup() {
   ]);
 
   initLocale();
-
-  // BasicExampleFactory.registerPrefs();
-
-  // BasicExampleFactory.registerNotifier();
-
-  // KeyExampleFactory.registerShortcuts();
-
-  // await UIExampleFactory.registerExtraColumn();
-
-  // await UIExampleFactory.registerExtraColumnWithCustomCell();
-
-  // UIExampleFactory.registerItemPaneCustomInfoRow();
-
-  // UIExampleFactory.registerItemPaneSection();
-
-  // UIExampleFactory.registerReaderItemPaneSection();
-
   await Promise.all(
     Zotero.getMainWindows().map((win) => onMainWindowLoad(win)),
   );
@@ -70,20 +53,6 @@ async function onMainWindowLoad(win: _ZoteroTypes.MainWindow): Promise<void> {
     progress: 30,
     text: `[30%] ${getString('startup-begin')}`,
   });
-
-  // UIExampleFactory.registerStyleSheet(win);
-
-  // UIExampleFactory.registerRightClickMenuItem();
-
-  // UIExampleFactory.registerRightClickMenuPopup(win);
-
-  // UIExampleFactory.registerWindowMenuWithSeparator();
-
-  // PromptExampleFactory.registerNormalCommandExample();
-
-  // PromptExampleFactory.registerAnonymousCommandExample(win);
-
-  // PromptExampleFactory.registerConditionalCommandExample();
 
   await Zotero.Promise.delay(1000);
 
@@ -121,68 +90,20 @@ async function onNotify(
   extraData: { [key: string]: any },
 ) {
   // You can add your code to the corresponding notify type
-  ztoolkit.log('notify', event, type, ids, extraData);
-  // if (
-  //   event == "select" &&
-  //   type == "tab" &&
-  //   extraData[ids[0]].type == "reader"
-  // ) {
-  //   BasicExampleFactory.exampleNotifierCallback();
-  // } else {
-  //   return;
-  // }
+  // ztoolkit.log('notify', event, type, ids, extraData);
 }
 
 /**
  * This function is just an example of dispatcher for Preference UI events.
- * Any operations should be placed in a function to keep this funcion clear.
+ * Any operations should be placed in a function to keep this function clear.
  * @param type event type
  * @param data event data
  */
-async function onPrefsEvent(type: string, data: { [key: string]: any }) {
-  // switch (type) {
-  //   case "load":
-  //     registerPrefsScripts(data.window);
-  //     break;
-  //   default:
-  //     return;
-  // }
-}
+async function onPrefsEvent(type: string, data: { [key: string]: any }) {}
 
-function onShortcuts(type: string) {
-  // switch (type) {
-  //   case "larger":
-  //     KeyExampleFactory.exampleShortcutLargerCallback();
-  //     break;
-  //   case "smaller":
-  //     KeyExampleFactory.exampleShortcutSmallerCallback();
-  //     break;
-  //   default:
-  //     break;
-  // }
-}
+function onShortcuts(type: string) {}
 
-function onDialogEvents(type: string) {
-  // switch (type) {
-  //   case "dialogExample":
-  //     HelperExampleFactory.dialogExample();
-  //     break;
-  //   case "clipboardExample":
-  //     HelperExampleFactory.clipboardExample();
-  //     break;
-  //   case "filePickerExample":
-  //     HelperExampleFactory.filePickerExample();
-  //     break;
-  //   case "progressWindowExample":
-  //     HelperExampleFactory.progressWindowExample();
-  //     break;
-  //   case "vtableExample":
-  //     HelperExampleFactory.vtableExample();
-  //     break;
-  //   default:
-  //     break;
-  // }
-}
+function onDialogEvents(type: string) {}
 
 // Add your hooks here. For element click, etc.
 // Keep in mind hooks only do dispatch. Don't add code that does real jobs in hooks.
