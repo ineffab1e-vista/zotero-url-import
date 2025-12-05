213595f56c1dc77d41095043ac02a5486b6f454c
Hanzhi Yin <yinhanzhi@gmail.com>
2025-12-05 14:26:11 +0800

    remove unnecessary hooks

--- src/hooks.ts
+++ src/hooks.ts
@@ -1,14 +1,4 @@
 import { registerURLImportHandler } from './modules/import';
-import { getString, initLocale } from './utils/locale';
-import { createZToolkit } from './utils/ztoolkit';
-// import {
-//   BasicExampleFactory,
-//   HelperExampleFactory,
-//   KeyExampleFactory,
-//   PromptExampleFactory,
-//   UIExampleFactory,
-// } from "./modules/examples";
-// import { registerPrefsScripts } from "./modules/preferenceScript";
 
 async function onStartup() {
   await Promise.all([
@@ -16,82 +6,25 @@ async function onStartup() {
     Zotero.unlockPromise,
     Zotero.uiReadyPromise,
   ]);
-
-  initLocale();
   await Promise.all(
     Zotero.getMainWindows().map((win) => onMainWindowLoad(win)),
   );
 
   registerURLImportHandler();
-
-  // Mark initialized as true to confirm plugin loading status
-  // outside of the plugin (e.g. scaffold testing process)
-  addon.data.initialized = true;
 }
 
-async function onMainWindowLoad(win: _ZoteroTypes.MainWindow): Promise<void> {
-  // Create ztoolkit for every window
-  addon.data.ztoolkit = createZToolkit();
+async function onMainWindowLoad(win: _ZoteroTypes.MainWindow): Promise<void> {}
 
-  win.MozXULElement.insertFTLIfNeeded(
-    `${addon.data.config.addonRef}-mainWindow.ftl`,
-  );
+async function onMainWindowUnload(win: Window): Promise<void> {}
 
-  const popupWin = new ztoolkit.ProgressWindow(addon.data.config.addonName, {
-    closeOnClick: true,
-    closeTime: -1,
-  })
-    .createLine({
-      text: getString('startup-begin'),
-      type: 'default',
-      progress: 0,
-    })
-    .show();
+function onShutdown(): void {}
 
-  await Zotero.Promise.delay(1000);
-  popupWin.changeLine({
-    progress: 30,
-    text: `[30%] ${getString('startup-begin')}`,
-  });
-
-  await Zotero.Promise.delay(1000);
-
-  popupWin.changeLine({
-    progress: 100,
-    text: `[100%] ${getString('startup-finish')}`,
-  });
-  popupWin.startCloseTimer(5000);
-
-  addon.hooks.onDialogEvents('dialogExample');
-}
-
-async function onMainWindowUnload(win: Window): Promise<void> {
-  ztoolkit.unregisterAll();
-  addon.data.dialog?.window?.close();
-}
-
-function onShutdown(): void {
-  ztoolkit.unregisterAll();
-  addon.data.dialog?.window?.close();
-  // Remove addon object
-  addon.data.alive = false;
-  // @ts-expect-error - Plugin instance is not typed
-  delete Zotero[addon.data.config.addonInstance];
-}
-
-/**
- * This function is just an example of dispatcher for Notify events.
- * Any operations should be placed in a function to keep this funcion clear.
- */
 async function onNotify(
   event: string,
   type: string,
   ids: Array<string | number>,
   extraData: { [key: string]: any },
-) {
-  // You can add your code to the corresponding notify type
-  // ztoolkit.log('notify', event, type, ids, extraData);
-}
+) {}
 
 /**
  * This function is just an example of dispatcher for Preference UI events.
@@ -104,10 +37,6 @@ async function onPrefsEvent(type: string, data: { [key: string]: any }) {}
 function onShortcuts(type: string) {}
 
 function onDialogEvents(type: string) {}
-
-// Add your hooks here. For element click, etc.
-// Keep in mind hooks only do dispatch. Don't add code that does real jobs in hooks.
-// Otherwise the code would be hard to read and maintain.
 
 export default {
   onStartup,
